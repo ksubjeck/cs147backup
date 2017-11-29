5434a70157f2b268f7fe82cd65c338704b55c038
Austin Flores <acflores.stanford.edu>
2017-11-28 19:56:55 -0800

    First Commit

---
+++ vTag/InventoryViewController.swift
@@ -0,0 +1,35 @@
+//
+//  InventoryViewController.swift
+//  vTag
+//
+//  Created by Austin-Caleb Flores on 11/28/17.
+//  Copyright © 2017 VTag. All rights reserved.
+//
+
+import UIKit
+
+class InventoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
