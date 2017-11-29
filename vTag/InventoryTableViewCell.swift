bdccda55927178d22e981e49a599ea331237dcbd
Austin Flores <acflores.stanford.edu>
2017-11-29 13:06:25 -0800

    Table View now functional. Have to fix color scheme for it and need to implement an inventory class for backend

---
+++ vTag/InventoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  InventoryTableViewCell.swift
+//  vTag
+//
+//  Created by Austin-Caleb Flores on 11/29/17.
+//  Copyright © 2017 VTag. All rights reserved.
+//
+
+import UIKit
+
+class InventoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
