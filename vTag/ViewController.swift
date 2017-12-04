a1c3552aff265cee7044072394cef7ec2deb9928
Austin Flores <acflores.stanford.edu>
2017-12-03 19:03:18 -0800

    Added AR functionality so that clicking on screen adds a plane facing you. Clicking on plane deletes it.

--- vTag/ViewController.swift
+++ vTag/ViewController.swift
@@ -13,6 +13,8 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var nodeModel:SCNNode!
+    let nodeName = "ship"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,19 +24,99 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
+        //sceneView.debugOptions = ARSCNDebugOptions.showWorldOrigin
+        //sceneView.debugOptions = ARSCNDebugOptions.showFeaturePoints
+        sceneView.antialiasingMode = .multisampling4X
+        
+        
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        //let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene();
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        let tagScene = SCNScene(named: "art.scnassets/ship.scn")!
+        nodeModel = tagScene.rootNode.childNode(withName: nodeName, recursively: true);
+        
     }
+    
+    
+    /**
+     *When user touches the screen
+    */
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let location = touches.first!.location(in: sceneView);
+        
+        //Checks if tag was pressed
+        var hitTestOptions = [SCNHitTestOption: Any]()
+        hitTestOptions[SCNHitTestOption.boundingBoxOnly] = true
+        let hitResults: [SCNHitTestResult]  = sceneView.hitTest(location, options: hitTestOptions)
+        if let hit = hitResults.first {
+            if let node = getParent(hit.node) {
+                node.removeFromParentNode()
+                return
+            }
+        }
+        
+        //Wants to add a tag and rotate to face you
+        let hitResultsFeaturePoints: [ARHitTestResult] =
+            sceneView.hitTest(location, types: .featurePoint)
+        if let hit = hitResultsFeaturePoints.first {
+            // Get a transformation matrix with the euler angle of the camera
+            let rotate = simd_float4x4(SCNMatrix4MakeRotation(sceneView.session.currentFrame!.camera.eulerAngles.y, 0, 1, 0))
+            
+            // Combine both transformation matrices
+            let finalTransform = simd_mul(hit.worldTransform, rotate)
+            
+            // Use the resulting matrix to position the anchor
+            sceneView.session.add(anchor: ARAnchor(transform: finalTransform))
+            // sceneView.session.add(anchor: ARAnchor(transform: hit.worldTransform))
+        }
+        
+
+        
+    }
+    
+    
+    /**
+    * Uses recursion to find parent node
+    */
+    func getParent(_ nodeFound: SCNNode?) -> SCNNode? {
+        if let node = nodeFound {
+            if node.name == nodeName {
+                return node
+            } else if let parent = node.parent {
+                return getParent(parent)
+            }
+        }
+        return nil
+    }
+    
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if !anchor.isKind(of: ARPlaneAnchor.self) {
+            DispatchQueue.main.async {
+                let modelClone = self.nodeModel.clone()
+                modelClone.position = SCNVector3Zero
+                
+                // Add model as a child of the node
+                node.addChildNode(modelClone)
+            }
+        }
+    }
+    
+    
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        
+        //configuration.planeDetection = .horizontal
 
         // Run the view's session
         sceneView.session.run(configuration)
