2468288315d898979b44055a4c73eeeae98f8f78
Keaton Subjeck <Keaton@DN0a235767.SUNet>
2017-12-04 15:13:49 -0800

    Added .gitignore file, as well as FriendsTableViews

---
+++ vTag/FriendsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendsTableViewCell.swift
+//  vTag
+//
+//  Created by Keaton Subjeck on 12/3/17.
+//  Copyright © 2017 VTag. All rights reserved.
+//
+
+import UIKit
+
+class FriendsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
